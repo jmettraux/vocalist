038b3959081b7509a5923f4e2515b7bc92c0eb2e
John Mettraux <jmettraux@gmail.com>
2015-03-09 17:15:12 +0900

    click kanji to go to next card

--- www/code.js
+++ www/code.js
@@ -3,8 +3,33 @@ var Vl = (function() {
 
   var self = this;
 
+  this.goToNext = function(event) {
+    console.log(event);
+    var index =
+      parseInt(
+        document.getElementById('card').getAttribute('data-vl-index'),
+        10);
+    Vl.displayEntry(index + 1);
+  };
+
+  this.displayEntry = function(index) {
+
+    var entry = document.querySelectorAll('#list > .entry')[index];
+    if ( ! entry) { Vl.displayEntry(0); return; }
+
+    var card = document.getElementById('card');
+    card.innerHTML = '';
+    card.appendChild(entry.cloneNode(true));
+    card.setAttribute('data-vl-index', index);
+
+    document.querySelector('#card .j').addEventListener('click', Vl.goToNext);
+  };
+
   this.ready = function() {
-    console.log("ready.");
+
+    var index = 0;
+
+    Vl.displayEntry(index);
   };
 
   //
