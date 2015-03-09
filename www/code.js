980ef8972a204c42af09d66f493ba7295e09e43e
John Mettraux <jmettraux@gmail.com>
2015-03-09 17:42:14 +0900

    implement Vl.showRuby()

--- www/code.js
+++ www/code.js
@@ -4,12 +4,23 @@ var Vl = (function() {
   var self = this;
 
   this.goToNext = function(event) {
-    console.log(event);
+    //console.log(event.target);
     var index =
       parseInt(
         document.getElementById('card').getAttribute('data-vl-index'),
         10);
     Vl.displayEntry(index + 1);
+  };
+
+  this.showRuby = function(event) {
+    //console.log(event);
+    //console.log(event.target);
+    if (event.target.classList.contains('j')) return;
+    var r = document.querySelector('#card .r');
+    var e = document.querySelector('#card .e');
+    if (r.style.display !== 'block') { r.style.display = 'block'; return; }
+    if (e.style.display !== 'block') { e.style.display = 'block'; return; }
+    Vl.displayEntry(0);
   };
 
   this.displayEntry = function(index) {
@@ -23,6 +34,7 @@ var Vl = (function() {
     card.setAttribute('data-vl-index', index);
 
     document.querySelector('#card .j').addEventListener('click', Vl.goToNext);
+    document.querySelector('#card').addEventListener('click', Vl.showRuby);
   };
 
   this.ready = function() {
