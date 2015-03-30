c881a8e4e1ca10687772147be7fc32dfd1e8b120
John Mettraux <jmettraux@gmail.com>
2015-03-31 07:35:29 +0900

    add more content

--- www/content.js
+++ www/content.js
@@ -1,1 +1,1 @@
-var cards = [["保護者","ほごしゃ","gardian, protector, patron"],["影響","えいきょう","influence, effect"],["怒り","いかり","anger, hatred, rage, wrath"],["怒る","おこる","to get angry/mad"],["筆者","ひっしゃ","writer, author"]];
+var cards = [["﻿保護者","ほごしゃ","gardian, protector, patron"],["影響","えいきょう","influence, effect"],["怒り","いかり","anger, hatred, rage, wrath"],["怒る","おこる","to get angry/mad"],["筆者","ひっしゃ","writer, author"],["のみならず","のみならず","besides, as well"],["拝見させていただきます","はいけんさせていただきます","let me look (?)"]];
