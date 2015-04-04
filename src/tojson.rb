36d94010e9eb17af4b75cb267ef4d19a8f11e78a
John Mettraux <jmettraux@gmail.com>
2015-04-05 06:54:52 +0900

    fix tojson.rb issue (dropping last card)

--- src/tojson.rb
+++ src/tojson.rb
@@ -7,11 +7,11 @@ a = []
 File.readlines(File.dirname(__FILE__) + '/source.txt').each do |line|
   line = line.strip
   next if line[0] == '#'
+  a << line
   if a.length == 3
     items << a
     a = []
   end
-  a << line
 end
 
 puts "var cards = #{JSON.dump(items)};"
